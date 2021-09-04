5595a1f674d7f7165272e1899311bc0e798e25e3
Manish Khedekar <manish@tillpos.com.au>
2021-09-04 16:31:27 +0530

    Padding changed

--- src/components/roadmap-section/roadmap-section.js
+++ src/components/roadmap-section/roadmap-section.js
@@ -7,12 +7,12 @@ import roadmap from '../../images/roadmap.svg';
 function RoadmapSection() {
   return (
     <div className="w-full roadmap-section">
-      <p className="container w-3/4 mx-auto py-8 text-5xl font-semibold leading-tight mb-6">
+      <p className="container w-3/4 mx-auto py-16 text-5xl font-semibold leading-tight mb-6">
           Roadmap
       </p>
     
       <div class="container w-3/4 mx-auto my-auto flex justify-between">
-        <div className="w-1/2 py-24">
+        <div className="w-1/2 py-16">
             <div className="flex my-8">
                 <div className="mr-8">
                     <img src={featureIcon1} alt="Feature Icon 1"/>
